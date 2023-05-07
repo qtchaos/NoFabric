1871e7a0f6ad5c55063822201345f28ac2d1ca62
qtchaos <72168435+qtchaos@users.noreply.github.com>
2023-05-07 18:55:12 +0300

    Bump Minecraft version to 1.19.4

--- src/main/java/dev/chaos/nofabric/CustomPayloadC2SMixin.java
+++ src/main/java/dev/chaos/nofabric/CustomPayloadC2SMixin.java
@@ -1,7 +1,7 @@
 package dev.chaos.nofabric;
 
 import net.minecraft.network.ClientConnection;
-import net.minecraft.network.Packet;
+import net.minecraft.network.packet.Packet;
 import net.minecraft.network.PacketCallbacks;
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import org.jetbrains.annotations.Nullable;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
 public class CustomPayloadC2SMixin {
-    @Inject(at = @At("HEAD"), method = "send(Lnet/minecraft/network/Packet;Lnet/minecraft/network/PacketCallbacks;)V", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;)V", cancellable = true)
     public void send(Packet<?> packet, @Nullable PacketCallbacks callbacks, CallbackInfo ci) {
         if(packet instanceof CustomPayloadC2SPacket) {
             ci.cancel();
